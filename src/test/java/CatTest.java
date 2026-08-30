4d6ec2d8f147a740c247f35b822be9c4961ca6f6
Eva Konovalova <eva12092002@yandex.ru>
2026-08-30 11:08:00 +0300

    Splitting checks for the Cat class

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -3,13 +3,12 @@ import com.example.Feline;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.times;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.when;
 
 public class CatTest {
@@ -35,18 +34,13 @@ public class CatTest {
     public void testGetFoodReturnList() throws Exception {
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
         when(mockFeline.eatMeat()).thenReturn(expectedFood);
-
         List<String> actualFood = cat.getFood();
-
         assertEquals(expectedFood, actualFood);
-        Mockito.verify(mockFeline, times(1)).eatMeat();
     }
 
     @Test
     public void testGetFoodException() throws Exception {
-        when(mockFeline.eatMeat()).thenThrow(new Exception("Ошибка в Feline"));
-
-        Exception exception = org.junit.Assert.assertThrows(Exception.class, () -> cat.getFood());
-        assertEquals("Ошибка в Feline", exception.getMessage());
+        when(mockFeline.eatMeat()).thenThrow(new Exception());
+        assertThrows(Exception.class, () -> cat.getFood());
     }
 }
